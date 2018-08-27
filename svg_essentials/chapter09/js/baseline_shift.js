2ea318c107c5fb4be2ef750a6dae03ff9695ed8a
Richard Myatt <lydgate77@hotmail.co.uk>
2018-08-27 13:59:53 +0100

    spreadMethod demo, inkscape ship and trial background.  Also continued to develop media icons.

---
+++ svg_essentials/chapter09/js/baseline_shift.js
@@ -0,0 +1,9 @@
+/*          Author: Richard Myatt
+            Date: 25 August 2018
+
+            An example of the use of baseline-shift based on
+            a similar example in 'SVG Essentials' by J.
+            David Eisenberg.
+*/
+
+alert("The code shown on this page demonstrates the use of SVG's baseline-shift.  Please note that this does not render properly in Firefox but does appear to work well in Chrome and the SL app.");
---
+++ svg_essentials/chapter09/js/vertical_text.js
@@ -0,0 +1,8 @@
+/*            Author: Richard Myatt
+              Date: 26 August 2018
+
+              An example of the use of vertical text in SVG taken from a similar
+              example in chapter 9 of 'SVG Essentials' by J. David Eisenberg.
+*/
+
+alert("This demonstration of the use of vertical text in SVG does not appear to render correctly within the browser.");
