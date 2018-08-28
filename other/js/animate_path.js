ec369df2d765992e6a281fda95b35ec05e77a1c7
Richard Myatt <lydgate77@hotmail.co.uk>
2018-08-28 20:07:05 +0100

    animated line demo uploaded

---
+++ other/js/animate_path.js
@@ -0,0 +1,13 @@
+/*          Author: Richard Myatt
+            Date: 28 August 2018
+
+            Exploring SVG animation techniques.  A first line
+            animation using css.
+*/
+
+/*var path = document.querySelector("svg path");
+var length = path.getTotalLength();
+console.log(length);*/
+
+// use the code above to find the total length of the
+// path.  In this case 247.78018188476562
